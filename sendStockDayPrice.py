7e655fee1918ff33a42b9c97e3ff6011662da0e5
chenhan20 <a0911558945@gmail.com>
2020-12-22 21:04:53 +0800

    修改每天收盤價格式 跟fix寫死日期了

--- sendStockDayPrice.py
+++ sendStockDayPrice.py
@@ -6,16 +6,16 @@ dateStr = datetime.datetime.now().strftime("%Y%m%d")
 
 
 def sendStockDayPrice():
-    stockPriceList = three.getStockPrice('20201221')
+    stockPriceList = three.getStockPrice(dateStr)
     if(len(stockPriceList) != 0):
         str = dateStr + '個股收盤\n'
         for stock in stockPriceList:
-            stockName = '<code>' + stock[0] + '(' + stock[1]+ ')</code>'
+            stockName = '<code>' + stock[0] + stock[1]+ '</code>'
             price = stock[8]
             chgPrefix = converterPrefix(stock[9])
             chg = stock[10]
             chgPercent = float(chg) / float(price) * 100
-            strTemp = stockName + '：<b>' + price + '</b>[' + chgPrefix + chg  +'][{:.2f}%'.format(chgPercent) +']\n'
+            strTemp = stockName + ':<b>' + price + '</b>(' + chgPrefix + chg  +' | {:.2f}%'.format(chgPercent) +')\n'
             str = str + strTemp
         telegramBot.sendMessage(str.replace('', ''))
     else:
