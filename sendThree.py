8fb8ef7055503c56a4400efaa4e90287cf0b5196
chenhan20 <a0911558945@gmail.com>
2021-08-11 23:21:41 +0800

    三大法人買賣超更新

--- sendThree.py
+++ sendThree.py
@@ -7,17 +7,25 @@ dateStr = datetime.datetime.now().strftime("%Y%m%d")
 
 def sendThree():
     threeData = three.getThree(dateStr)
-    str = dateStr + '三大法人買賣超\n'
+    str = '<b>' + dateStr + '三大法人買賣超</b>\n'
     if(len(threeData['data']) != 0):
+        print( threeData) 
         str = str + threeData['fields'][0] + \
             ' : ' + threeData['fields'][3] + '\n'
         for data in threeData['data']:
             tempStr = '<code>' + data[0] + '</code> : <b>' + data[3] + '</b>'
+            if(data[0]=='合計'):
+                tempStr += '\n -----------------------------------'
+                tempStr += '\n<b>總買' + data[1] + '  總賣' + data[2] + '</b>'
             str = str + tempStr + '\n'
+
+        # 測試用這個 
+        # telegramBot.newSendMessage(str, '919045167')
 
         telegramIds = getDb.getTelegramIds()
         for id in telegramIds:
             telegramBot.newSendMessage(str, id)
+            
     else:
         print(dateStr + '查無資料')
 
