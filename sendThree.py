7f31dc5fe39d1cd258fd068007f3a180743bf045
chenhan20 <a0911558945@gmail.com>
2021-08-21 12:43:39 +0800

    三大法人買賣 使用prettyTable 發

--- sendThree.py
+++ sendThree.py
@@ -2,35 +2,36 @@ import getStockThree as three
 import datetime
 import telegramBot
 import getDb
+import prettytable as pt
 
 dateStr = datetime.datetime.now().strftime("%Y%m%d")
 
 def sendThree():
     threeData = three.getThree(dateStr)
-    str = '<b>' + dateStr + '三大法人買賣超</b>\n'
+    tb1 = pt.PrettyTable()
+    tb1.set_style(pt.DEFAULT)
+    title = dateStr + '三大法人買賣超'
     if(len(threeData['data']) != 0):
-        str = str + threeData['fields'][0] + \
-            ' : ' + threeData['fields'][3] + '\n'
+        col1 = threeData['fields'][0]
+        col2 = threeData['fields'][3]
+        tb1.field_names = [col1,col2]
+        tb1.align[col1] = "c"
+        tb1.align[col2] = "r"
         for data in threeData['data']:
-            tempStr = ''
-            if(data[0] != '合計'):
-                tempStr += '<code>' + data[0] + '</code> : <b>' + data[3] + '</b>'
-            else:
-                tempStr += '--------------------------------------'
-                tempStr += '\n<b>總買</b><code>' + data[1] + '</code>  <b>總賣</b><code>' + data[2] + '</code>'
-                tempStr += '\n--------------------'
-                tempStr += '\n| <code>' + data[0] + '</code> : <b>' + data[3] + '</b> |'
-                tempStr += '\n--------------------'
+            name = data[0].replace("(", "").replace(")", "");
+            dataCol_1 = name[0:2]
+            dataCol_2 = data[3]
+            tb1.add_row([dataCol_1, dataCol_2])
                 
-            str = str + tempStr + '\n'
-            
-        # print(str)
+        # print(tb1.get_string(title=title))
+        tbStr = '<pre>' + tb1.get_string(title=title) + '</pre>'
+
         # 測試用這個 
-        # telegramBot.newSendMessage(str, '919045167')
+        # telegramBot.newSendMessage(tbStr, '919045167')
 
         telegramIds = getDb.getTelegramIds()
         for id in telegramIds:
-            telegramBot.newSendMessage(str, id)
+            telegramBot.newSendMessage(tbStr, id)
             
     else:
         print(dateStr + '查無資料')
