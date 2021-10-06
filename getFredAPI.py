fc2248aa9abaa33ee9c112d788dd5625cb94c8db
chenhan20 <a0911558945@gmail.com>
2021-10-06 21:10:14 +0800

    commit 排版

--- getFredAPI.py
+++ getFredAPI.py
@@ -20,10 +20,13 @@ def getFredAPI():
         watchLastInfo = fred.get_series_info(watch)
         watchLastIndex = fred.get_series(watch).tail(2)
         lastUpdateDate = watchLastInfo.observation_end
-        percent = round((watchLastIndex[1] - watchLastIndex[0]) / watchLastIndex[0] * 100 , 2) 
+        indexGap = round(watchLastIndex[1] - watchLastIndex[0], 2)
+        percent = round(indexGap / watchLastIndex[0] * 100 , 2) 
         result['title'] = watch
         result['value'] = str(watchLastIndex[1])
-        result['percent'] = converterPrefix(percent)
+        result['indexGap'] = converterPrefix(indexGap)
+        converterPercent = str(percent).replace('+','').replace('-','')
+        result['gapPercent'] = converterPercent + '%'
         result['lastUpdateDate'] = lastUpdateDate
         resultList.append(result)
 
@@ -37,28 +40,8 @@ def converterPrefix(percent):
         prefix = '🔺'
     elif(percent < 0):
         prefix = '🔻'
-    converterValue = prefix + converterPercent + '%'
+    converterValue = prefix + converterPercent
     return converterValue
 
-def testFredAPI():
-    fred = Fred(api_key=fredToken) 
-    watchList = ['SP500','DJIA','NASDAQCOM']
-    resultList = []
-    for watch in watchList:
-        result = dict()
-        watchLastInfo = fred.get_series_info(watch)
-        watchLastIndex = fred.get_series(watch).tail(2)
-        lastUpdateDate = watchLastInfo.observation_end
-        if(len(watchLastIndex)>1):
-            percent = round((watchLastIndex[1] - watchLastIndex[0]) / watchLastIndex[0] * 100 , 2) 
-            result['title'] = watch
-            result['value'] = str(watchLastIndex[1])
-            result['percent'] = converterPrefix(percent)
-            result['lastUpdateDate'] = lastUpdateDate
-            resultList.append(result)
-    
-    return resultList    
-
 if __name__ == '__main__':
-    print(testFredAPI())
-    # print(testFredAPI())
+    print(getFredAPI())
