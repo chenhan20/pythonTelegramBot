75d123bcb95c143188450bbe8142f3032c9d5d6e
chenhan20 <a0911558945@gmail.com>
2021-10-25 00:05:54 +0800

    add crypot訂閱

--- getDb.py
+++ getDb.py
@@ -159,8 +159,8 @@ def addUser(fromUser):
         cur = conn.cursor()
         if(len(account) == 0 ):
             print('new accounts')
-            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled)' 
-            sql += ' VALUES (%s, now(), now(), %s, true, true, true)'
+            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled, telegram_push_crypto_enabled)' 
+            sql += ' VALUES (%s, now(), now(), %s, true, true, true, true)'
             cur.execute(sql, (fromUser.first_name, fromUser.id))
         else:
             print('old accounts')
@@ -177,7 +177,7 @@ def removeUser(fromUser):
     try:
         conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
         cur = conn.cursor()
-        sql = 'UPDATE accounts SET telegram_push_enabled = False, telegram_push_tw_enabled = False, telegram_push_us_enabled = False, last_login = now() WHERE telegram_user_id = (%s)' 
+        sql = 'UPDATE accounts SET telegram_push_enabled = False, telegram_push_tw_enabled = False, telegram_push_us_enabled = False, telegram_push_crypto_enabled = False, last_login = now() WHERE telegram_user_id = (%s)' 
         cur.execute(sql, (str(fromUser.id),))
         conn.commit()       
 
@@ -192,8 +192,8 @@ def enabledTw(fromUser, enabled):
         cur = conn.cursor()
         if(len(account) == 0 ):
             print('new accounts')
-            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled)' 
-            sql += ' VALUES (%s, now(), now(), %s, true, true, true)'
+            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled, telegram_push_crypto_enabled)' 
+            sql += ' VALUES (%s, now(), now(), %s, true, true, true, true)'
             cur.execute(sql, (fromUser.first_name, fromUser.id))
         else:
             print('old accounts')
@@ -212,12 +212,32 @@ def enabledUs(fromUser, enabled):
         cur = conn.cursor()
         if(len(account) == 0 ):
             print('new accounts')
-            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled)' 
-            sql += ' VALUES (%s, now(), now(), %s, true, true, true)'
+            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled, telegram_push_crypto_enabled)' 
+            sql += ' VALUES (%s, now(), now(), %s, true, true, true, true)'
             cur.execute(sql, (fromUser.first_name, fromUser.id))
         else:
             print('old accounts')
             sql = 'UPDATE accounts SET telegram_push_us_enabled = %s, last_login = now() WHERE telegram_user_id = (%s)' 
+            cur.execute(sql, (enabled, str(fromUser.id),))
+        conn.commit()       
+
+    except Exception as error:
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
+        
+def enabledCrypto(fromUser, enabled):
+    try:
+        account = getAccount(fromUser.id)
+        conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+        cur = conn.cursor()
+        if(len(account) == 0 ):
+            print('new accounts')
+            sql = 'INSERT INTO accounts(username, created_on, last_login, telegram_user_id, telegram_push_enabled, telegram_push_tw_enabled, telegram_push_us_enabled, telegram_push_crypto_enabled)' 
+            sql += ' VALUES (%s, now(), now(), %s, true, true, true, true)'
+            cur.execute(sql, (fromUser.first_name, fromUser.id))
+        else:
+            print('old accounts')
+            sql = 'UPDATE accounts SET telegram_push_crypto_enabled = %s, last_login = now() WHERE telegram_user_id = (%s)' 
             cur.execute(sql, (enabled, str(fromUser.id),))
         conn.commit()       
 
