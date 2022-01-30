ad00b5ae2bdd044fbf174b64f274267331788489
chenhan20 <a0911558945@gmail.com>
2022-01-30 22:48:43 +0800

    在再不分==

--- receiveMessage.py
+++ receiveMessage.py
@@ -16,7 +16,7 @@ bot = telebot.TeleBot(token)
 def start (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Hi! {}歡迎你使用SteveBot 服務 \n今日起將會收到全球股市資訊 若不想在收到 請輸入\n/end", fromUser.first_name))
+    bot.reply_to(message,  str.format("Hi! {}歡迎你使用SteveBot 服務 \n今日起將會收到全球股市資訊 若不想再收到 請輸入\n/end", fromUser.first_name))
     isNewUser = getDb.addUser(fromUser)
     if isNewUser:
         telegramBot.sendMessageForSteve('[ADMIN] new accounts name:' + fromUser.first_name)
@@ -26,49 +26,49 @@ def start (message):
 def end (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Thank! {} 感謝你的使用 已暫停所有訂閱通知 若想在收到 請輸入\n/start 或是個別訂閱", fromUser.first_name))
+    bot.reply_to(message,  str.format("Thank! {} 感謝你的使用 已暫停所有訂閱通知 若想再收到 請輸入\n/start 或是個別訂閱", fromUser.first_name))
     getDb.removeUser(fromUser)
 
 @bot.message_handler(commands=['startTW'])
 def start (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到台股資訊 若不想在收到 請輸入\n/endTW", fromUser.first_name))
+    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到台股資訊 若不想再收到 請輸入\n/endTW", fromUser.first_name))
     getDb.enabledTw(fromUser, True)
 
 @bot.message_handler(commands=['endTW'])
 def end (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停台股訂閱通知 若想在收到 請輸入\n/startTW 或是全部訂閱/start", fromUser.first_name))
+    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停台股訂閱通知 若想再收到 請輸入\n/startTW 或是全部訂閱/start", fromUser.first_name))
     getDb.enabledTw(fromUser, False)
 
 @bot.message_handler(commands=['startUS'])
 def start (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到美股資訊 若不想在收到 請輸入\n/endUS", fromUser.first_name))
+    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到美股資訊 若不想再收到 請輸入\n/endUS", fromUser.first_name))
     getDb.enabledUs(fromUser, True)
 
 @bot.message_handler(commands=['endUS'])
 def end (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停美股訂閱通知 若想在收到 請輸入\n/startUS 或是全部訂閱\n/start", fromUser.first_name))
+    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停美股訂閱通知 若想再收到 請輸入\n/startUS 或是全部訂閱\n/start", fromUser.first_name))
     getDb.enabledUs(fromUser, False)
 
 @bot.message_handler(commands=['startCrypto'])
 def start (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到加密貨幣資訊 若不想在收到 請輸入\n/endCrypto", fromUser.first_name))
+    bot.reply_to(message,  str.format("Hi! {} \n今日起將會收到加密貨幣資訊 若不想再收到 請輸入\n/endCrypto", fromUser.first_name))
     getDb.enabledCrypto(fromUser, True)
 
 @bot.message_handler(commands=['endCrypto'])
 def end (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停加密貨幣訂閱通知 若想在收到 請輸入\n/startCrypto 或是全部訂閱\n/start", fromUser.first_name))
+    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停加密貨幣訂閱通知 若想再收到 請輸入\n/startCrypto 或是全部訂閱\n/start", fromUser.first_name))
     getDb.enabledCrypto(fromUser, False)
     
 @bot.message_handler(commands=['announcement'])
