af025a548f7b4d65df2733ddbe343ad80fd07921
steve.chuang <a0911558945@gmail.com>
2024-10-19 17:23:13 +0800

    改抓資料庫資料發送

--- receiveMessage.py
+++ receiveMessage.py
@@ -83,7 +83,7 @@ def start (message):
 def end (message):
     fromUser = message.from_user
     print(fromUser.last_name + fromUser.first_name + '(' + str(fromUser.id) + ') : ' + message.text)
-    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停COSTCO商品特價資訊訂閱通知 若想再收到 請輸入\n/startCostco 或是全部訂閱\n/startCostco", fromUser.first_name))
+    bot.reply_to(message,  str.format("Thank you! {} 感謝你的使用 已暫停COSTCO商品特價資訊訂閱通知 若想再收到 請輸入\n/startCostco", fromUser.first_name))
     getDb.enabledCostco(fromUser, False)
     
 @bot.message_handler(commands=['announcement'])
